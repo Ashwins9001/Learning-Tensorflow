a8cb5cde19f5e375aa99a52860985878c8a40a62
ashwins9001 <nitromekid@hotmail.com>
2020-03-08 21:13:04 +0530

    RNN input setup

--- RNN_Sentiment_Analysis.py
+++ RNN_Sentiment_Analysis.py
@@ -31,3 +31,24 @@ print(tokenizer.word_index)
 #Convert words in test/training set to token form where freq of each shown
 x_train_tokens = tokenizer.texts_to_sequences(x_train_text)
 print(np.array(x_train_tokens[1]))
+x_test_tokens = tokenizer.texts_to_sequences(x_test_text)
+
+#Require seq to have same length, RNN can take arbitrary length seq in, yet mem wastage if longest seq used
+#Either ensure all seq of data-set are same length, or write custom data-gen that ensures seq eq length w/ in batch
+#Use avg seq length to cover most, truncate long, pad short
+#Length token isn't each to num of words in seq, but rather unique words in seq 
+num_tokens = [len(tokens) for tokens in x_train_tokens + x_test_tokens] #arr of token lengths for each text seq
+num_tokens = np.array(num_tokens)
+print(np.mean(num_tokens)) #mean token length : 221.76
+print(np.max(num_tokens)) #max token length : 2209
+max_tokens = np.mean(num_tokens) + 2 * np.std(num_tokens) #max is mean plus two std dev
+max_tokens = int(max_tokens)
+print(max_tokens) #max allowable tokens : 544
+
+#Order of padding & truncating matters, zeros added for pad, integers thrown away for trunc
+#Pre or post = throw away FIRST or LAST and pad BEG or END respectively
+pad = 'pre'
+x_train_pad = pad_sequences(x_train_tokens, maxlen = max_tokens, padding = pad, truncating = pad)
+x_test_pad = pad_sequences(x_test_tokens, maxlen = max_tokens, padding = pad, truncating = pad)
+print(x_train_pad.shape) #shape (25000, 544), 25000 word seq which are tokenized to int-form, and each has a len 544
+print(x_train_pad[1])
